109875ac5ab7361a210d9e95c19b5b1c8bb8c5b3
Gautam Kumar <gautamkumar43421@gmail.com>
2026-02-04 18:59:28 +0530

    chore: Remove legacy Express code from repo

--- apps/api/src/routes/public.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { resumeController } from "../controllers/resume.controller.js";
-
-const router = Router();
-
-// Public route for viewing shared resumes
-router.get("/:shareId", resumeController.getPublicResume);
-
-export default router;
