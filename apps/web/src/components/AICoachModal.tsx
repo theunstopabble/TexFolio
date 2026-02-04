a9f60503f63371aa23494efd98a8f9aeddf354e1
Gautam Kumar <gautamkumar43421@gmail.com>
2026-02-04 19:10:47 +0530

    chore: Fix duplicate import error

--- apps/web/src/components/AICoachModal.tsx
+++ apps/web/src/components/AICoachModal.tsx
@@ -1,6 +1,6 @@
 import { Dialog, Transition } from "@headlessui/react";
 import { Fragment } from "react";
-import { useResumeCoach, useResumeCoach } from "../hooks/useResumeCoach";
+import { useResumeCoach } from "../hooks/useResumeCoach";
 
 interface AICoachModalProps {
   isOpen: boolean;
